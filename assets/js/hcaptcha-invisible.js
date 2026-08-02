559cc196e6ecf8849ca2ccabe0c2604d1c6a17ec
Velmurugan Kuberan <vkuberan@gmail.com>
2026-08-02 20:41:25 +0530

    feat: support hCaptcha invisible

---
+++ assets/js/hcaptcha-invisible.js
@@ -0,0 +1,84 @@
+(function () {
+  "use strict";
+
+  var WIDGET_SELECTOR = ".wp-captcha-shield-hcaptcha-invisible-widget";
+
+  function initialize(container) {
+    var formId = container.dataset.formId;
+    var siteKey = container.dataset.siteKey;
+    var form = document.getElementById(formId);
+    var widgetId = null;
+    var requestingToken = false;
+    var submitter = null;
+
+    if (!form || !siteKey) {
+      return;
+    }
+
+    widgetId = hcaptcha.render(container, {
+      sitekey: siteKey,
+      size: "invisible",
+      callback: function () {
+        requestingToken = false;
+        resumeSubmission(form, submitter);
+      },
+      "expired-callback": function () {
+        requestingToken = false;
+      },
+      "error-callback": function () {
+        requestingToken = false;
+      },
+      "close-callback": function () {
+        requestingToken = false;
+      },
+    });
+
+    form.addEventListener("submit", function (event) {
+      if (widgetId !== null && hcaptcha.getResponse(widgetId) !== "") {
+        return;
+      }
+
+      event.preventDefault();
+
+      if (requestingToken || widgetId === null) {
+        return;
+      }
+
+      requestingToken = true;
+      submitter = event.submitter || null;
+
+      try {
+        hcaptcha.execute(widgetId);
+      } catch (error) {
+        requestingToken = false;
+      }
+    });
+  }
+
+  function resumeSubmission(form, submitter) {
+    if (typeof form.requestSubmit === "function") {
+      if (submitter) {
+        form.requestSubmit(submitter);
+        return;
+      }
+
+      form.requestSubmit();
+      return;
+    }
+
+    form.submit();
+  }
+
+  window.wpCaptchaShieldHCaptchaOnload = function () {
+    if (
+      typeof hcaptcha === "undefined" ||
+      typeof hcaptcha.render !== "function" ||
+      typeof hcaptcha.execute !== "function" ||
+      typeof hcaptcha.getResponse !== "function"
+    ) {
+      return;
+    }
+
+    document.querySelectorAll(WIDGET_SELECTOR).forEach(initialize);
+  };
+})();
--- src/WordPress/Forms/Captcha/HCaptchaWidget.php
+++ src/WordPress/Forms/Captcha/HCaptchaWidget.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace WpCaptchaShield\WordPress\Forms\Captcha;
 
 use WpCaptchaShield\Domain\Configuration\CaptchaProvider;
+use WpCaptchaShield\Domain\Configuration\Provider\HCaptchaDisplayMode;
 use WpCaptchaShield\WordPress\Settings\PluginSettings;
 
 final class HCaptchaWidget implements CaptchaWidget
 {
     private const TOKEN_FIELD = 'h-captcha-response';
+
+    private const INVISIBLE_WIDGET_CLASS =
+        'wp-captcha-shield-hcaptcha-invisible-widget';
 
     public function provider(): CaptchaProvider
     {
@@ -20,7 +24,28 @@ final class HCaptchaWidget implements CaptchaWidget
         CaptchaWidgetContext $context,
         PluginSettings $settings,
     ): void {
-        unset($context, $settings);
+        unset($context);
+
+        if ($settings->hCaptcha()->mode() === HCaptchaDisplayMode::Invisible) {
+            wp_enqueue_script(
+                'wp-captcha-shield-hcaptcha-invisible',
+                WP_CAPTCHA_SHIELD_URL . 'assets/js/hcaptcha-invisible.js',
+                [],
+                WP_CAPTCHA_SHIELD_VERSION,
+                true,
+            );
+
+            wp_enqueue_script(
+                'wp-captcha-shield-hcaptcha',
+                'https://js.hcaptcha.com/1/api.js?'
+                . 'onload=wpCaptchaShieldHCaptchaOnload&render=explicit',
+                ['wp-captcha-shield-hcaptcha-invisible'],
+                null,
+                true,
+            );
+
+            return;
+        }
 
         wp_enqueue_script(
             'wp-captcha-shield-hcaptcha',
@@ -35,11 +60,24 @@ final class HCaptchaWidget implements CaptchaWidget
         CaptchaWidgetContext $context,
         PluginSettings $settings,
     ): void {
-        unset($context);
+        $hCaptcha = $settings->hCaptcha();
+
+        if ($hCaptcha->mode() === HCaptchaDisplayMode::Invisible) {
+            printf(
+                '<div id="%s" class="%s" '
+                . 'data-form-id="%s" data-site-key="%s"></div>',
+                esc_attr($this->invisibleWidgetId($context)),
+                esc_attr(self::INVISIBLE_WIDGET_CLASS),
+                esc_attr($context->formId()),
+                esc_attr($hCaptcha->siteKey()),
+            );
+
+            return;
+        }
 
         printf(
             '<div class="h-captcha" data-sitekey="%s"></div>',
-            esc_attr($settings->hCaptcha()->siteKey()),
+            esc_attr($hCaptcha->siteKey()),
         );
     }
 
@@ -48,5 +86,12 @@ final class HCaptchaWidget implements CaptchaWidget
         unset($settings);
 
         return self::TOKEN_FIELD;
+    }
+
+    private function invisibleWidgetId(
+        CaptchaWidgetContext $context,
+    ): string {
+        return 'wp-captcha-shield-hcaptcha-invisible-widget-'
+            . sanitize_html_class($context->formId());
     }
 }
